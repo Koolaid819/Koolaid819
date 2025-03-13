11fdd8c866c11035ef5869faff0e8a8c85f285c3
Koolaid819 <kbonev845@gmail.com>
2025-03-13 15:24:02 +0200

    Update Reply.js

--- models/Reply.js
+++ models/Reply.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const replySchema = new mongoose.Schema({
   text: { type: String, required: true },
-  created_on: { type: Date, default: Date.now },
-  thread_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Thread' },
   delete_password: { type: String, required: true },
+  created_on: { type: Date, default: Date.now },
   reported: { type: Boolean, default: false },
+  thread_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Thread', required: true }
 });
 
 module.exports = mongoose.model('Reply', replySchema);
