36bc400101edead1d1d9fd46a40408ab0e9af1cd
Koolaid819 <kbonev845@gmail.com>
2025-03-13 15:23:49 +0200

    Update Thread.js

--- models/Thread.js
+++ models/Thread.js
@@ -2,18 +2,15 @@ const mongoose = require('mongoose');
 
 const threadSchema = new mongoose.Schema({
   text: { type: String, required: true },
+  delete_password: { type: String, required: true },
   created_on: { type: Date, default: Date.now },
   bumped_on: { type: Date, default: Date.now },
   reported: { type: Boolean, default: false },
-  delete_password: { type: String, required: true },
-  replies: [
-    {
-      text: { type: String, required: true },
-      created_on: { type: Date, default: Date.now },
-      delete_password: { type: String, required: true },
-      reported: { type: Boolean, default: false },
-    },
-  ],
+  replies: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Reply'
+  }],
+  board: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Thread', threadSchema);
